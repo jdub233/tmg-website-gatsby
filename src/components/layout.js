a1c2791340e703546ce35ef8ea7545f4be10efb1
Unknown <tamlin@mac.com>
2020-05-24 17:26:49 -0400

    :sparkles:add footer to layout

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from './layout/header';
+import Footer from "./layout/footer";
 
 export default function Layout({ children }) {
     return (
         <div>
             <Header />
             {children}
+            <Footer />
         </div>
     );
 }
---
+++ src/components/layout/footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+import { Link } from 'gatsby';
+
+export default () => (
+    <div>
+        <hr />
+        <Link to="/">Tangible Media Group</Link>
+    </div>
+);
