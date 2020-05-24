13e58aaaccc3ff317a6d832dbe0761a33d85ed37
Unknown <tamlin@mac.com>
2020-05-24 13:54:05 -0400

    add papers list to people

--- src/templates/person.js
+++ src/templates/person.js
@@ -5,12 +5,15 @@ import Layout from "../components/layout";
 
 const Person = ({ data: { allPeopleJson: { edges } } }) => {
   const person = edges[0].node;
+  const papers = edges[0].node.portalData.PeoplePaperJoin;
 
   return (
     <Layout>
       <Link to='/people/'><h2>People</h2></Link>
       <h3>{person.fieldData.Full_Name}</h3>
       <div dangerouslySetInnerHTML={{ __html: person.fieldData.DescriptionHTML }} />
+      <h4>Papers</h4>
+      {papers.map((node) => <div key={node.Papers__ID}>{node.Papers__Title}</div>)}
     </Layout>
   );
 };
@@ -30,6 +33,13 @@ export const query = graphql`
             BadgeContainerURL
             Title
             cBadgeRawURL
+          }
+          portalData {
+            PeoplePaperJoin {
+              Papers__PaperID
+              Papers__Title
+              Papers__SC_published_pdf_Download_URL
+            }
           }
         }
       }
