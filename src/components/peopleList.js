3f05e4d8ef73f8def1b1e5e7ebd288a4398af6a8
Unknown <tamlin@mac.com>
2020-05-24 00:38:14 -0400

    refactor people list

--- src/components/peopleList.js
+++ src/components/peopleList.js
@@ -2,36 +2,36 @@ import React from "react"
 import { StaticQuery, graphql, Link } from "gatsby"
 
 const PeopleList = () => (
-    <StaticQuery
-        query={graphql`
-      {
-        allPeopleJson {
-          edges {
-            node {
-              id
-              fieldData {
-                slug
-                Full_Name
-                cBadgeRawURL
-                Category
-              }
+  <StaticQuery
+    query={graphql`
+    {
+      allPeopleJson {
+        edges {
+          node {
+            id
+            fieldData {
+              slug
+              Full_Name
+              cBadgeRawURL
+              Category
             }
           }
         }
       }
-    `}
-        render={data => (
-            <div>
-                {data.allPeopleJson.edges.map( ({ node }) => (
-                    <div key={node.id} >
-                      <Link to={`/people/${node.fieldData.slug}`}>
-                        {node.fieldData.Full_Name}
-                      </Link>
-                    </div>
-                ) )}
-            </div>
-        )}
-    ></StaticQuery>
+    }
+  `}
+    render={({ allPeopleJson: {edges} }) => { return(
+        <div>
+            {edges.map( ({ node }) => (
+                <div key={node.id} >
+                  <Link to={`/people/${node.fieldData.slug}`}>
+                    {node.fieldData.Full_Name}
+                  </Link>
+                </div>
+            ) )}
+        </div>
+    );}}
+  ></StaticQuery>
 )
 
 export default PeopleList
