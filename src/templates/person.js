85f9b51f363ada4331c7f3f4684d48b9aa967925
Unknown <tamlin@mac.com>
2020-05-24 00:22:25 -0400

    :sparkles:more formatting for project and person
    detail pages

--- src/templates/person.js
+++ src/templates/person.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout";
 
-const Person = ({ data }) => <Layout><pre>{JSON.stringify(data, null, 4)}</pre></Layout>
+const Person = ({ data: { allPeopleJson: { edges } } }) => {
+  const person = edges[0].node;
+
+  return (
+    <Layout>
+      <Link to='/people/'><h2>People</h2></Link>
+      <h3>{person.fieldData.Full_Name}</h3>
+      <div dangerouslySetInnerHTML={{ __html: person.fieldData.DescriptionHTML }} />
+    </Layout>
+  );
+};
 
 export const query = graphql`
   query($slug: String!) {
--- src/templates/project.js
+++ src/templates/project.js
@@ -1,9 +1,19 @@
 import React from "react"
-import { graphql } from "gatsby"
+import { graphql, Link } from "gatsby"
 
 import Layout from "../components/layout";
 
-const Project = ({ data }) => <Layout><pre>{JSON.stringify(data, null, 4)}</pre></Layout>
+const Project = ({ data: { allProjectsJson: { edges } } }) => {
+  const project = edges[0].node;
+
+  return(
+    <Layout>
+      <h2><Link to='/projects/'>Projects</Link></h2>
+      <h3>{project.fieldData.Name}</h3>
+      <div dangerouslySetInnerHTML={{ __html: project.fieldData.DescriptionHTML }} />
+    </Layout>
+  )
+};
 
 export const query = graphql`
   query($slug: String!) {
