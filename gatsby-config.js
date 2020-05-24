f3d733186157b8b5b814539a88a2fbf55a5dd621
Unknown <tamlin@mac.com>
2020-05-24 18:08:00 -0400

    :sparkles:add sass
    we :heart: sass

--- gatsby-config.js
+++ gatsby-config.js
@@ -18,5 +18,6 @@ module.exports = {
         path: `${__dirname}/data/`,
       },
     },
+    `gatsby-plugin-sass`,
   ],
 }
--- src/components/layout.js
+++ src/components/layout.js
@@ -1,11 +1,14 @@
 import React from 'react';
+
+import "./layout.scss";
+
 import Header from './layout/header';
 import Footer from "./layout/footer";
 
 export default function Layout({ children }) {
     return (
-        <div>
-            <Header />
+        <div id="container">
+            <Header className="header"/>
             {children}
             <Footer />
         </div>
