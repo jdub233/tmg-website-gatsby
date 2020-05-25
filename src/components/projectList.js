fcf0cf0580336f2203fad1fa314ae0d70630f785
Unknown <tamlin@mac.com>
2020-05-25 15:49:44 -0400

    :sparkles:group projects by year

--- src/components/projectList.js
+++ src/components/projectList.js
@@ -21,25 +21,40 @@ const ProjectList = () => {
         }
       }
     }
-  `)
-    return (
-        
-        <div>
-            {data.allProjectsJson.edges.map( ({ node }) => (
-                <div key={node.id}>
-                <Link to={`/projects/${node.fieldData.slug}`}>
-                    <img 
-                      width="140px" 
-                      alt="{node.fieldData.Name}"
-                      src={`${process.env.MEDIA_LIBRARY}/${node.fieldData.cBadgeRawURL}?width=140`} 
-                    />
-                    {node.fieldData.Name}
-                </Link>
-                </div>
-            ) )}
-        </div>
-        
-    );
+  `);
+
+  const { allProjectsJson: { edges: projects } } = data;
+
+  const projectsByYearObj = projects.reduce( (accumulator, { node }) => {
+    accumulator[node.fieldData.Project_Year] = [...accumulator[node.fieldData.Project_Year] || [], node ];
+    return accumulator;
+  }, {} );
+
+  return( 
+    Object.entries(projectsByYearObj).reverse().map(([key, projects]) => (
+    <div key={key}>
+      <h3>{key}</h3>
+      <ProjectYearList projects={projects} />
+    </div>
+    ))
+  );
 }
+
+const ProjectYearList = ( {projects} ) => (
+  <div>
+    {projects.map(( node ) => (
+      <div key={node.id}>
+        <Link to={`/projects/${node.fieldData.slug}`}>
+          <img
+            width="140px"
+            alt="{node.fieldData.Name}"
+            src={`${process.env.MEDIA_LIBRARY}/${node.fieldData.cBadgeRawURL}?width=140`}
+          />
+          {node.fieldData.Name}
+        </Link>
+      </div>
+    ))}
+  </div>
+);
 
 export default ProjectList
