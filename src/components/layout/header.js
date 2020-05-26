5d8aec64de5db84522e3e8b4c2812e4e3af4771e
Unknown <tamlin@mac.com>
2020-05-25 23:51:14 -0400

    more rough formatting for header

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -5,18 +5,18 @@ import "./header.scss";
 
 export default () => (
     <div className="header">
-        <div className="supertitle"><a href="https://media.mit.edu">MIT Media Lab</a></div>
-        <h1><Link to="/">Tangible Media Group</Link></h1>
-        <div className="navbar">
-            <ul>
-                <li><Link activeClassName="active" to='/vision/'>vision</Link></li>
-                <li><Link activeClassName="active" to='/people/'>people</Link></li>
-                <li><Link activeClassName="active" to='/projects/'>projects</Link></li>
-                <li><Link activeClassName="active" to='/papers/'>papers</Link></li>
-                <li><Link activeClassName="active" to='/events/'>events</Link></li>
-                <li><Link activeClassName="active" to='/awards/'>press &#38; awards</Link></li>
-                <li><Link activeClassName="active" to='/about/'>about &#38; contact</Link></li>
-            </ul>
+        <div className="logo">
+            <div className="supertitle"><a href="https://media.mit.edu">MIT Media Lab</a></div>
+            <h1><Link to="/">Tangible Media Group</Link></h1>
         </div>
+        <ul className="navbar">
+            <li><Link activeClassName="active" to='/vision/'>vision</Link></li>
+            <li><Link activeClassName="active" to='/people/'>people</Link></li>
+            <li><Link activeClassName="active" to='/projects/'>projects</Link></li>
+            <li><Link activeClassName="active" to='/papers/'>papers</Link></li>
+            <li><Link activeClassName="active" to='/events/'>events</Link></li>
+            <li><Link activeClassName="active" to='/awards/'>press &#38; awards</Link></li>
+            <li><Link activeClassName="active" to='/about/'>about &#38; contact</Link></li>
+        </ul>
     </div>
 );
