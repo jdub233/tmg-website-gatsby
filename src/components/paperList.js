a463a03e42482f5fdf41634af1d17a8b366805a1
Unknown <tamlin@mac.com>
2020-05-25 22:30:53 -0400

    add project links to papers

--- src/components/paperList.js
+++ src/components/paperList.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { StaticQuery, graphql } from "gatsby";
+import { StaticQuery, graphql, Link } from "gatsby";
 
 import "./paperList.scss";
 import pdfIcon from "../img/pdf-icon.png";
@@ -25,6 +25,13 @@ const PaperList = () => (
                 Title
                 Venue
               }
+              portalData {
+                proj_portal {
+                  Projects_forPapers__cBadgeRawURL
+                  Projects_forPapers__Name
+                  Projects_forPapers__slug
+                }
+              }
             }
           }
         }
@@ -42,14 +49,24 @@ const PaperList = () => (
             <h3>{key}</h3>
             {papers.map((node) => (
               <div className="paperBox" key={node.id}>
-                <div><img src={pdfIcon} alt="download pdf icon" /></div>
+                <div><a href={`${process.env.MEDIA_LIBRARY}/${node.fieldData.SC_published_pdf_Download_URL}`}><img src={pdfIcon} alt="download pdf icon" /></a></div>
                 <div className="citation">
                   <a href={`${process.env.MEDIA_LIBRARY}/${node.fieldData.SC_published_pdf_Download_URL}`}>
                     <h4>{node.fieldData.Title}</h4>
                     <p>{node.fieldData.Citation}</p>
                   </a>
                 </div>
-                <div>projects</div>
+                <div>
+                  {node.portalData.proj_portal.map( (project) => (
+                    <Link to={`/projects/${project.Projects_forPapers__slug}`}>
+                      <img 
+                        src={`${process.env.MEDIA_LIBRARY}/${project.Projects_forPapers__cBadgeRawURL}?width=60`} 
+                        width="60px"
+                        alt={project.Projects_forPapers__Name}
+                      />
+                    </Link>
+                  ) )}
+                </div>
               </div>
             ))}
           </div>
