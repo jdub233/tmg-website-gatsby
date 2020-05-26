1e39c0f0166aa3332c9f0d2a5421f3e43d12314a
Unknown <tamlin@mac.com>
2020-05-26 01:25:14 -0400

    list events by year

--- src/components/eventsList.js
+++ src/components/eventsList.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const EventsList = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     {
       allEventsJson {
         edges {
@@ -21,8 +21,33 @@ const EventsList = () => {
         }
       }
     }
-  `)
-    return <pre>{JSON.stringify(data, null, 4)}</pre>
+  `);
+  
+  const { allEventsJson: { edges: events } } = data;
+
+  // Group events by year with reduce.
+  const eventsByYearObj = events.reduce( (accumulator, { node }) => {
+    accumulator[node.fieldData.Event_Year] = [ ...accumulator[node.fieldData.Event_Year] || [], node ];
+    return accumulator;
+  }, {} );
+
+  return (
+    Object.entries(eventsByYearObj).reverse().map( ([key, events]) => (
+      <div key={key}>
+        <h3>{key}</h3>
+        <EventYearList events={events} />
+      </div>
+    ) )
+    
+  );
 }
+
+const EventYearList = ( {events} ) => (
+  <div className="eventYearList">
+    {events.map( ( node ) => (
+      <div>{node.fieldData.Title}</div>
+    ) )}
+  </div>
+);
 
 export default EventsList
