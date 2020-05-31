2d3951eb125807d906f7d106a4b487de09529363
Unknown <tamlin@mac.com>
2020-05-31 17:24:31 -0400

    basic people list layout

--- src/components/peopleList.js
+++ src/components/peopleList.js
@@ -1,11 +1,18 @@
-import React from "react"
-import { StaticQuery, graphql, Link } from "gatsby"
+import React from "react";
+import { StaticQuery, graphql, Link } from "gatsby";
 
-const PeopleListItem = (props) => (
-  <div key={props.node.id} >
-    <Link to={`/people/${props.node.fieldData.slug}`}>
-      {props.node.fieldData.Full_Name}
+import "./peopleList.scss";
+
+const PeopleListItem = ( {node} ) => (
+  <div className="person" key={node.id} >
+    <Link to={`/people/${node.fieldData.slug}`}>
+      <img src={`${process.env.MEDIA_LIBRARY}/${node.fieldData.cBadgeRawURL}?width=140`} />
     </Link>
+    <div className="description">
+      <h4>{node.fieldData.Full_Name}</h4>
+      <div>{node.fieldData.Category}</div>
+      <div>{node.fieldData.SubCategory}</div>
+    </div>
   </div>
 );
 
@@ -22,6 +29,7 @@ const PeopleList = () => (
               Full_Name
               cBadgeRawURL
               Category
+              SubCategory
             }
           }
         }
@@ -41,39 +49,63 @@ const PeopleList = () => (
 
       return(
         <div>
-          <h4>Professor</h4>
-          {prof.map(({ node }) => (
-            <PeopleListItem node={node} />
+          <h3>Professor</h3>
+          <div className="people-list">
+            {prof.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Ph.D. Candidates</h3>
+          <div className="people-list">
+            {phds.map( ({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>M.S. Candidates</h3>
+          <div className="people-list">
+            {masters.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Visiting Scientists</h3>
+          <div className="people-list">
+            {visitingScientist.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Administrative Assistant</h3>
+          <div className="people-list">
+            {admin.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Research Affiliate</h3>
+          <div className="people-list">
+            {affiliate.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Collaborator</h3>
+          <div className="people-list">
+            {collab.map(({ node }) => (
+              <PeopleListItem node={node} />
           ))}
-          <h4>Ph.D. Candidates</h4>
-          {phds.map( ({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>M.S. Candidates</h4>
-          {masters.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Visiting Scientists</h4>
-          {visitingScientist.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Administrative Assistant</h4>
-          {admin.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Research Affiliate</h4>
-          {affiliate.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Collaborator</h4>
-          {collab.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Visiting Student</h4>
-          {visitingStudent.map(({ node }) => (
-            <PeopleListItem node={node} />
-          ))}
-          <h4>Alumni</h4>
+          </div>
+
+          <h3>Visiting Student</h3>
+          <div className="people-list">
+            {visitingStudent.map(({ node }) => (
+              <PeopleListItem node={node} />
+            ))}
+          </div>
+
+          <h3>Alumni</h3>
           {alumni.map(({ node }) => (
             <PeopleListItem node={node} />
           ))}
