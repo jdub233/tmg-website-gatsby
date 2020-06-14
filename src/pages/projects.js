17170b2db5e958301a86db584c77e88afcfe77b1
Unknown <tamlin@mac.com>
2020-06-14 16:59:07 -0400

    remove artifacts

--- src/pages/projects.js
+++ src/pages/projects.js
@@ -3,7 +3,7 @@ import React from "react";
 import Layout from "../components/layout";
 import ProjectList from "../components/projectList";
 
-export default (data) => (
+export default () => (
   <Layout>
     <div>
         <h2>Projects</h2>
