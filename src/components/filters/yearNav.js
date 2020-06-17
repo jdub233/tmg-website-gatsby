e07c46642da019f716d3fe5735e2f7433d52f6a1
Unknown <tamlin@mac.com>
2020-06-16 22:38:12 -0400

    convert year links to buttons
    and add override styles

--- src/components/filters/yearNav.js
+++ src/components/filters/yearNav.js
@@ -6,12 +6,12 @@ export default ({ years, setYear, currentYear }) => (
     <ul className="yearNav">
         {years.map((y) => (
             <li>
-                <a
-                    href="#" onClick={() => setYear(y)}
+                <button
+                    onClick={() => setYear(y)}
                     className={`year${(y === currentYear) ? '-selected' : ''}`}
                 >
                     {y}
-                </a>
+                </button>
             </li>
         ))}
     </ul>
