23015b983aabb2ddc6a231606169d3f6665a846c
Unknown <tamlin@mac.com>
2020-06-20 19:13:25 -0400

    build out project view with person css

--- src/templates/project.js
+++ src/templates/project.js
@@ -1,18 +1,26 @@
 import React from "react"
-import { graphql, Link } from "gatsby"
+import { graphql } from "gatsby"
 
 import Layout from "../components/layout";
+import NormalizeP from "../components/filters/normalizeP";
+
+import "./person.scss"
 
 const Project = ({ data: { allProjectsJson: { edges } } }) => {
-  const project = edges[0].node;
+  const project = edges[0].node.fieldData;
   const papers = edges[0].node.portalData.ProjectPaperJoin_displayProject;
 
   return(
     <Layout>
-      <h2><Link to='/projects/'>Projects</Link></h2>
-      <h3>{project.fieldData.Name}</h3>
-      <div>{project.fieldData.Members}</div>
-      <div dangerouslySetInnerHTML={{ __html: project.fieldData.DescriptionHTML }} />
+      <h2>{project.Name} <span className="person-category">{project.Members}</span></h2>
+      <div className="person-details">
+        <img 
+          className="badge" 
+          alt={project.Name} 
+          src={`${process.env.MEDIA_LIBRARY}/${project.cBadgeRawURL}?width=140`}
+        />
+        <NormalizeP className="description" mixedMarkup={project.DescriptionHTML} />
+      </div>
       <h4>Papers</h4>
       {papers.map((node) => (
         <div key={node.recordId}>
