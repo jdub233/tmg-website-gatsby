451eacbdf4f0d6654652ae8f9cda4f21b28e6af6
Unknown <tamlin@mac.com>
2020-06-20 19:19:33 -0400

    initial content for vision page

--- src/pages/vision.js
+++ src/pages/vision.js
@@ -1,5 +1,4 @@
 import React from "react";
-import { Link } from 'gatsby';
 
 import "./vision.scss";
 
@@ -7,14 +6,22 @@ import Layout from '../components/layout';
 
 export default () => (
     <Layout>
-        <h2 className="vision-text">
-            The Tangible Media Group, led by
-            Professor Hiroshi Ishii, explores the
-            Tangible Bits &#38; Radical Atoms visions
-            to seamlessly couple the dual world
-            of bits and atoms by giving dynamic
-            physical form to digital information
-            and computation.
-        </h2>
+        <div className="vision-grid">
+            <h2 className="vision-text">
+                The Tangible Media Group, led by
+                Professor Hiroshi Ishii, explores the
+                Tangible Bits &#38; Radical Atoms visions
+                to seamlessly couple the dual world
+                of bits and atoms by giving dynamic
+                physical form to digital information
+                and computation.
+            </h2>
+            <div className="vision-image">
+                &nbsp;
+            </div>
+        </div>
+        <div className="vision-text">
+            
+        </div>
     </Layout>
 );
