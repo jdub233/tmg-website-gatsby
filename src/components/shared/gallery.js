af51172c8c14d3c92786e7019e66b14e0eb618dd
Unknown <tamlin@mac.com>
2020-06-27 17:13:41 -0400

    :sparkles: add state for clickable gallery nav

--- src/components/shared/gallery.js
+++ src/components/shared/gallery.js
@@ -1,25 +1,29 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./gallery.scss"
 
-export default ({ assets, name }) => (
-  <div className="gallery">
-    <h4>{name}</h4>
-    <img
-      className="gallery-main"
-      src={`${process.env.MEDIA_LIBRARY}/${assets[0].fieldData.sc_asset_relative_url}?width=780`}
-      alt={assets[0].fieldData.Title}
-    />
-    <div className="gallery-thumbnails">
-      {assets.map(({ fieldData: { AssetID, Title, sc_asset_relative_url } }) => (
-        <button className="thumbnail">
-          <img
-            key={AssetID}
-            src={`${process.env.MEDIA_LIBRARY}/${sc_asset_relative_url}?height=60`}
-            alt={Title}
-          />
-        </button>
-      ))}
+export default ({ assets, name }) => {
+  const [current, setCurrent] = useState(0);
+
+  return (
+    <div className="gallery">
+      <h4>{name}</h4>
+      <img
+        className="gallery-main"
+        src={`${process.env.MEDIA_LIBRARY}/${assets[current].fieldData.sc_asset_relative_url}?width=780`}
+        alt={assets[current].fieldData.Title}
+      />
+      <div className="gallery-thumbnails">
+        {assets.map(({ fieldData: { AssetID, Title, sc_asset_relative_url } }, index) => (
+          <button onClick={() => setCurrent(index)} className="thumbnail">
+            <img
+              key={AssetID}
+              src={`${process.env.MEDIA_LIBRARY}/${sc_asset_relative_url}?height=60`}
+              alt={Title}
+            />
+          </button>
+        ))}
+      </div>
     </div>
-  </div>
-);
+  )
+};
