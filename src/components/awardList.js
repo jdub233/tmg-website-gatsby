c15727c469c579fcc405b2ccea3f06178be40221
Unknown <tamlin@mac.com>
2020-06-28 15:22:34 -0400

    add press data query

--- src/components/awardList.js
+++ src/components/awardList.js
@@ -2,7 +2,7 @@ import React from "react"
 import { useStaticQuery, graphql } from "gatsby"
 
 const AwardList = () => {
-    const data = useStaticQuery(graphql`
+  const data = useStaticQuery(graphql`
     {
       allAwardsJson {
         edges {
@@ -24,17 +24,52 @@ const AwardList = () => {
           }
         }
       }
+      allPressJson {
+        edges {
+          node {
+            id
+            portalData {
+              proj_portal {
+                recordId
+                Projects_for_Press__ProjectID
+                Projects_for_Press__Name
+                Projects_for_Press__cBadgeRawURL
+                Projects_for_Press__internal_recid
+                Projects_for_Press__slug
+                modId
+              }
+            }
+            fieldData {
+              PDFDownloadURL
+              PressID
+              Published_In
+              Published_URL
+              Title
+              Type
+              isPDFPublic
+            }
+          }
+        }
+      }
     }
-  `)
-    return (
-        <div>
-            {data.allAwardsJson.edges.map( ({ node }) => (
-                <div key={node.id}>
-                    {node.fieldData.Title}
-                </div>
-            ) )}
+  `);
+
+  const { allAwardsJson: { edges: awards }, allPressJson: {edges: pressItems} } = data;
+
+  return (
+    <div>
+      {awards.map(({ node }) => (
+        <div key={node.id}>
+          {node.fieldData.Title}
         </div>
-    )
+      ))}
+      {pressItems.map(({ node }) => (
+        <div key={node.id}>
+          {node.fieldData.Title}
+        </div>
+      ))}
+    </div>
+  )
 }
 
 export default AwardList
