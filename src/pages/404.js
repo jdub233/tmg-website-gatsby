b18050549595207b5416ea4b54e5ec38251c959c
Unknown <tamlin@mac.com>
2020-07-03 00:15:20 -0400

    404 page content

--- src/pages/404.js
+++ src/pages/404.js
@@ -1,9 +1,13 @@
 import React from "react";
+import { Link } from "gatsby";
 
 import Layout from '../components/layout';
 
 export default () => (
     <Layout>
-        404 sorry not found!
+        <h2>404 sorry not found!</h2>
+        <p>
+            The link you requested could not be found. It may have been moved or deleted. If you have any questions, please <Link to="/contact-admissions/">contact us.</Link>
+        </p>
     </Layout>
 );
