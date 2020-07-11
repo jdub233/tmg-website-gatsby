ffe2a378c273b0af4802a7ef2cd4a6b9c6f16293
Unknown <tamlin@mac.com>
2020-07-11 11:32:16 -0400

    refactor people list around shared component
    for all the categories except Alumni, which gets rendered separately

--- src/components/peopleList.js
+++ src/components/peopleList.js
@@ -3,6 +3,31 @@ import { StaticQuery, graphql, Link } from "gatsby";
 
 import "./peopleList.scss";
 
+const categories = [
+  'Professor',
+  'Ph.D. Candidate',
+  'M.S. Candidate',
+  'Visiting Scientist',
+  'Administrative Assistant',
+  'Research Affiliate',
+  'Collaborator',
+  'Visiting Student',
+  //'Alumni', //handle alumni separately
+];
+
+
+const CategoryList = ( {category, people} ) => (
+  <div>
+    <h3>{category}</h3>
+    <div className="people-list">
+      {people.map(({ node }) => (
+        <PeopleListItem node={node} />
+      ))}
+    </div>
+  </div>
+)
+
+
 const PeopleListItem = ({ node: { id, fieldData } }) => (
   <div className="person" key={id} >
     <Link to={`/people/${fieldData.slug}`}>
@@ -46,73 +71,13 @@ const PeopleList = () => (
     }
   `}
     render={({ allPeopleJson: {edges} }) => {
-      const prof     = edges.filter(({ node }) => node.fieldData.Category === 'Professor');
-      const phds     = edges.filter(({ node }) => node.fieldData.Category === 'Ph.D. Candidate');
-      const masters  = edges.filter(({ node }) => node.fieldData.Category === 'M.S. Candidate');
-      const visitingScientist = edges.filter(({ node }) => node.fieldData.Category === 'Visiting Scientist');
-      const admin    = edges.filter(({ node }) => node.fieldData.Category === 'Administrative Assistant');
-      const affiliate = edges.filter(({ node }) => node.fieldData.Category === 'Research Affiliate');
-      const collab = edges.filter(({ node }) => node.fieldData.Category === 'Collaborator');
-      const visitingStudent = edges.filter(({ node }) => node.fieldData.Category === 'Visiting Student');
+
       const alumni = edges.filter(({ node }) => node.fieldData.Category === 'Alumni');
+      const categorized = categories.map((category) => edges.filter(({ node }) => node.fieldData.Category === category ));
 
       return(
         <div>
-          <h3>Professor</h3>
-          <div className="people-list">
-            {prof.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>Ph.D. Candidates</h3>
-          <div className="people-list">
-            {phds.map( ({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>M.S. Candidates</h3>
-          <div className="people-list">
-            {masters.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>Visiting Scientists</h3>
-          <div className="people-list">
-            {visitingScientist.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>Administrative Assistant</h3>
-          <div className="people-list">
-            {admin.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>Research Affiliate</h3>
-          <div className="people-list">
-            {affiliate.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
-
-          <h3>Collaborator</h3>
-          <div className="people-list">
-            {collab.map(({ node }) => (
-              <PeopleListItem node={node} />
-          ))}
-          </div>
-
-          <h3>Visiting Student</h3>
-          <div className="people-list">
-            {visitingStudent.map(({ node }) => (
-              <PeopleListItem node={node} />
-            ))}
-          </div>
+          {categories.map( (category, index) => <CategoryList category={category} people={categorized[index]} /> )}
 
           <h3>Alumni</h3>
           <div className="alumni-list">
