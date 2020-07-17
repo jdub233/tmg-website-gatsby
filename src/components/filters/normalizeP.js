b1d0abe59e61f4da481f517f9a7debc2599e656a
Unknown <tamlin@mac.com>
2020-07-16 22:26:06 -0400

    add key with index
    because the order of paragraphs should never change

--- src/components/filters/normalizeP.js
+++ src/components/filters/normalizeP.js
@@ -4,7 +4,7 @@ export default ({ mixedMarkup, className }) => {
     // Renders carriage returns to <p> tags.
     const normalizedMarkup = mixedMarkup.split('\r')
         .filter(x => x !== "")
-        .map(graf => <p dangerouslySetInnerHTML={{ __html: graf }} />);
+        .map((graf, index) => <p key={index} dangerouslySetInnerHTML={{ __html: graf }} />);
 
     return (
         <div className={className}>{normalizedMarkup}</div>
