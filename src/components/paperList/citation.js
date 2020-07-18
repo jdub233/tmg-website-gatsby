40dea62630011b795000d36e8b2f3624222d3896
Unknown <tamlin@mac.com>
2020-07-18 15:17:51 -0400

    more citation styling

--- src/components/paperList/citation.js
+++ src/components/paperList/citation.js
@@ -12,7 +12,9 @@ export default ( {fieldData} ) => {
             <p>{fieldData.Citation}</p>
             <div className="citation-doi">DOI: {fieldData.DOI_URL}</div>
             <h5>
-                <button className="show-abstract" onClick={() => setShowAbstract(!showAbstract)}> Abstract {showAbstract ? '-' : '+'}</button>
+                <button className="show-abstract" onClick={() => setShowAbstract(!showAbstract)}> 
+                    Abstract <span className="show-abstract-icon">{showAbstract ? '-' : '+'}</span>
+                </button>
             </h5>
             <div className={`citation-abstract${ !showAbstract ? '-hide' : ''}`}>{fieldData.Abstract}</div>
         </div>
