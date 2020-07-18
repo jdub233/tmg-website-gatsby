567ce7060ee26448d7061fc37e7c5aa21e82fadc
Unknown <tamlin@mac.com>
2020-07-18 13:28:25 -0400

    use abstract display state
    and add more citation styling

--- src/components/paperList/citation.js
+++ src/components/paperList/citation.js
@@ -8,9 +8,15 @@ export default ( {fieldData} ) => {
         <div className="citation">
             <a href={`${process.env.MEDIA_LIBRARY}/${fieldData.Download_URL}`}>
                 <h4>{fieldData.Title}</h4>
-                <p>{fieldData.Citation}</p>
-                <p>DOI: {fieldData.DOI_URL}</p>
             </a>
+            <p>{fieldData.Citation}</p>
+            <p className="citation-doi">DOI: {fieldData.DOI_URL}</p>
+            <h5 onClick={() => setShowAbstract(!showAbstract)}>Abstract { showAbstract ? "-" : "+" }</h5>
+
+            { showAbstract &&
+                <div className="citation-abstract">{fieldData.Abstract}</div>
+            }
+
         </div>
     );
 };
