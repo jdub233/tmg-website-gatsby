3237e0503a025ea64742b1f77c56e14227a3a506
Unknown <tamlin@mac.com>
2020-08-02 00:32:53 -0400

    add state and arrays for stateful values
    while also dynamically generating buttons and imgs

--- src/components/vision/statement.js
+++ src/components/vision/statement.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 
 import VisionHeadline from "./visionHeadline";
 
@@ -8,6 +8,28 @@ import tui from "../../img/vision-image-tui.jpg";
 import ra from "../../img/vision-image-ra.jpg";
 
 export default () => {
+    const phases = [
+        'GUI',
+        'TUI',
+        'Radical Atoms',
+    ];
+
+    const phaseImages = [
+        gui,
+        tui,
+        ra,
+    ];
+
+    const [phase, setPhase] = useState('TUI');
+
+    const buttons = phases.map((x) => (
+        <button 
+            key={x} 
+            onClick={() => setPhase(x)} 
+            className={`slides-nav-item${(phase === x) ? '-selected' : ''}`}
+        >{x}</button>
+    ));
+
     return (
         <div className="vision-statement">
             <VisionHeadline />
@@ -16,11 +38,9 @@ export default () => {
                     <div className="atoms">Atoms</div>
                     <div className="bits">Bits</div>
                     <div className="slides-nav">
-                        <button className="slides-nav-item-selected">GUI</button>
-                        <button className="slides-nav-item">TUI</button>
-                        <button className="slides-nav-item">Radical Atoms</button>
+                        {buttons}
                     </div>
-                    <img alt="TUI iceberg" src={gui} />
+                    <img alt={phase} src={phaseImages[(phases.findIndex(a => a === phase))]} />
                 </div>
                 <div className="illustration-caption">
                     A <strong>Graphical User Interface</strong> only lets us see information and interact with it indirectly, as if we were looking through the surface of the water to interact with the forms below.
