18da07e1b2bcc2d1829fd74d1dcdc054077e2ae0
Unknown <tamlin@mac.com>
2020-08-12 15:57:35 -0400

    use getProps for people/person link
    to return the active class based on the path coming from the getProps args

--- src/components/layout/navbar.js
+++ src/components/layout/navbar.js
@@ -4,7 +4,12 @@ import { Link } from "gatsby";
 export default () => (
     <ul className="navbar">
         <li><Link activeClassName="active" to='/vision/'>vision</Link></li>
-        <li><Link activeClassName="active" partiallyActive={true} to='/people/'>people</Link></li>
+        <li>
+            <Link activeClassName="active"
+                getProps={ ({location: {pathname}}) => ( pathname.startsWith('/people') || pathname.startsWith('/person/') ? {className: "active"} : null ) } 
+                to='/people/'
+            >people</Link>
+        </li>
         <li><Link activeClassName="active" partiallyActive={true} to='/projects/'>projects</Link></li>
         <li><Link activeClassName="active" to='/papers/'>papers</Link></li>
         <li><Link activeClassName="active" to='/events/'>events</Link></li>
