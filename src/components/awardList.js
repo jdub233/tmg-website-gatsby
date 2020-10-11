05f2995799ee5b28a9d1aee958aa107a746a17ab
Unknown <tamlin@mac.com>
2020-10-10 20:44:36 -0400

    fix award title links

--- src/components/awardList.js
+++ src/components/awardList.js
@@ -117,7 +117,9 @@ const AwardBox = ( { award: { id, fieldData, portalData: { proj_portal: projects
       {fieldData.SummaryTitle && 
         <p className="award-box-tagline">{fieldData.SummaryTitle}</p>
       }
-      <h4>{fieldData.Title}</h4>
+      <h4>
+        {(fieldData.Award_URL === "") ? fieldData.Title : <a href={fieldData.Award_URL}>{fieldData.Title}</a>}
+      </h4>
       {fieldData.Description &&
         <p className="award-box-textblock">{fieldData.Description}</p>
       }
