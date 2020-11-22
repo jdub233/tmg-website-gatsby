d806dd30a2acb85c6bde005cd6d9627f13af7e2d
Unknown <tamlin@mac.com>
2020-11-21 21:17:15 -0500

    :art: eslint fixes

--- src/components/shared/formattedDate.js
+++ src/components/shared/formattedDate.js
@@ -1,17 +1,17 @@
-import React from "react";
+import React from 'react';
 
 export default ({ dateString }) => {
-    // Days are optional, so count the date segments by the '-' separator to see if the day should be displayed.
-    // Filter is useful here because it will always return an array, even an empty one, so will always have a valid length.
-    const dateSeparators = [...dateString].filter(x => x === '-').length;
+  // Days are optional, so count the date segments by the '-' separator to see if the day should be displayed.
+  // Filter is useful here because it will always return an array, even an empty one, so will always have a valid length.
+  const dateSeparators = [...dateString].filter((x) => x === '-').length;
 
-    const eventDate = new Date(dateString);
+  const eventDate = new Date(dateString);
 
-    return (
-        <>
-            { (dateSeparators > 0) && eventDate.toLocaleString('default', { timeZone: 'UTC', month: 'long' }) + ' '}
-            { (dateSeparators === 2) && eventDate.getUTCDate().toString() + ', '}
-            {eventDate.getUTCFullYear()}
-        </>
-    );
-}
+  return (
+    <>
+      { (dateSeparators > 0) && eventDate.toLocaleString('default', { timeZone: 'UTC', month: 'long' }) + ' '}
+      { (dateSeparators === 2) && eventDate.getUTCDate().toString() + ', '}
+      {eventDate.getUTCFullYear()}
+    </>
+  );
+};
