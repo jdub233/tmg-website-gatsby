9c2a39400839c9239f95e1bf61a916f2b9a6363d
Unknown <tamlin@mac.com>
2020-11-21 22:33:17 -0500

    :art: eslint fixes

--- src/components/shared/projectBoxes.js
+++ src/components/shared/projectBoxes.js
@@ -1,30 +1,30 @@
-import React from "react";
-import { Link } from "gatsby";
+import React from 'react';
+import { Link } from 'gatsby';
 
-import "./projectBoxes.scss";
+import './projectBoxes.scss';
 
-const NameSpan = ( {name} ) => {
-    if (name === 'Programmable Droplets for Interaction') { return <span>Programmable Droplets...</span>  }
-    if (name === 'TRANSFORM as Dynamic and Adaptive Furniture') { return <span>TRANSFORM as Dynamic...</span> }
-    if (name === 'Weight/Volume Changing UI') { return <span>Weight/Volume Changing UI</span> }
+const NameSpan = ({ name }) => {
+  if (name === 'Programmable Droplets for Interaction') { return <span>Programmable Droplets...</span>; }
+  if (name === 'TRANSFORM as Dynamic and Adaptive Furniture') { return <span>TRANSFORM as Dynamic...</span>; }
+  if (name === 'Weight/Volume Changing UI') { return <span>Weight/Volume Changing UI</span>; }
 
-    return <span>{name.length > 18 ? `${name.substring(0, 17)}...` : name}</span>
-}
+  return <span>{name.length > 18 ? `${name.substring(0, 17)}...` : name}</span>;
+};
 
 export default ({ projects }) => (
-    <div className="projectBoxes">
-        {projects.map((node) => (
-            <div className="projectItem" key={node.id}>
-                <Link className="projectBadge" to={`/project/${node.fieldData.slug}`}>
-                    <img
-                        alt="{node.fieldData.Name}"
-                        src={`${process.env.GATSBY_MEDIA_LIBRARY}/${node.fieldData.cBadgeRawURL}?width=140`}
-                    />
-                    <p className="projectBadge-title">
-                        <NameSpan name={node.fieldData.Name} />
-                    </p>
-                </Link>
-            </div>
-        ))}
-    </div>
+  <div className="projectBoxes">
+    {projects.map((node) => (
+      <div className="projectItem" key={node.id}>
+        <Link className="projectBadge" to={`/project/${node.fieldData.slug}`}>
+          <img
+            alt="{node.fieldData.Name}"
+            src={`${process.env.GATSBY_MEDIA_LIBRARY}/${node.fieldData.cBadgeRawURL}?width=140`}
+          />
+          <p className="projectBadge-title">
+            <NameSpan name={node.fieldData.Name} />
+          </p>
+        </Link>
+      </div>
+    ))}
+  </div>
 );
