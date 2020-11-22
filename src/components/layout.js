c9d90ef31c2a1e339274e3c79d914f55777584fe
Unknown <tamlin@mac.com>
2020-11-21 23:35:44 -0500

    :art: eslint fixes

--- src/components/layout.js
+++ src/components/layout.js
@@ -1,18 +1,18 @@
 import React from 'react';
 
-import "./layout.scss";
+import './layout.scss';
 
 import Header from './layout/header';
-import Footer from "./layout/footer";
+import Footer from './layout/footer';
 
 export default function Layout({ children }) {
-    return (
-        <div id="container">
-            <Header/>
-            <div className="content">
-                {children}
-            </div>
-            <Footer />
-        </div>
-    );
+  return (
+    <div id="container">
+      <Header />
+      <div className="content">
+        {children}
+      </div>
+      <Footer />
+    </div>
+  );
 }
