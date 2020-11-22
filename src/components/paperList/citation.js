8966223cfc6fc694e5c10923c3b1e20a15cd5096
Unknown <tamlin@mac.com>
2020-11-21 21:16:43 -0500

    :art: eslint fixes

--- src/components/paperList/citation.js
+++ src/components/paperList/citation.js
@@ -1,24 +1,35 @@
-import React, { useState } from "react";
-import "./citation.scss";
+import React, { useState } from 'react';
+import './citation.scss';
 
-export default ({ fieldData: { Download_URL, Title, Citation, DOI_URL, Abstract }} ) => {
-    const [showAbstract, setShowAbstract] = useState(false);
+export default ({
+  fieldData: {
+    Download_URL, Title, Citation, DOI_URL, Abstract,
+  },
+}) => {
+  const [showAbstract, setShowAbstract] = useState(false);
 
-    return (
-        <div className="citation">
-            <a href={`${process.env.GATSBY_MEDIA_LIBRARY}/${Download_URL}`}>
-                <h4>{Title}</h4>
-            </a>
-            <p>{Citation}</p>
-            {DOI_URL !== '' &&
-                <div className="citation-doi">DOI: {DOI_URL}</div>
-            }
-            <h5>
-                <button className="show-abstract" onClick={() => setShowAbstract(!showAbstract)}> 
-                    Abstract <span className="show-abstract-icon">{showAbstract ? '-' : '+'}</span>
-                </button>
-            </h5>
-            <div className={`citation-abstract${ !showAbstract ? '-hide' : ''}`}>{Abstract}</div>
+  return (
+    <div className="citation">
+      <a href={`${process.env.GATSBY_MEDIA_LIBRARY}/${Download_URL}`}>
+        <h4>{Title}</h4>
+      </a>
+      <p>{Citation}</p>
+      {DOI_URL !== ''
+        && (
+        <div className="citation-doi">
+          DOI:
+          {' '}
+          {DOI_URL}
         </div>
-    );
+        )}
+      <h5>
+        <button type="button" className="show-abstract" onClick={() => setShowAbstract(!showAbstract)}>
+          Abstract
+          {' '}
+          <span className="show-abstract-icon">{showAbstract ? '-' : '+'}</span>
+        </button>
+      </h5>
+      <div className={`citation-abstract${!showAbstract ? '-hide' : ''}`}>{Abstract}</div>
+    </div>
+  );
 };
