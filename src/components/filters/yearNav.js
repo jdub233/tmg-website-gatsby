ee1381b880f1391016840ec385003fbe3fcf8ed1
Unknown <tamlin@mac.com>
2020-11-21 20:59:59 -0500

    :art: eslint fixes

--- src/components/filters/yearNav.js
+++ src/components/filters/yearNav.js
@@ -1,26 +1,29 @@
-import React from "react";
+import React from 'react';
 
-import "./yearNav.scss";
+import './yearNav.scss';
 
-export default ({ years, setYear, currentYear, showPrevNext = false }) => (
-    <ul className="yearNav">
-        {showPrevNext && 
-            <li key="prev"><button className="prevNext" onClick={() => setYear( (currentYear === 1) ? (years.length) : currentYear - 1 )}>previous</button></li>
-        }
-        {years.map((y) => (
-            <li key={y}>
-                <button
-                    onClick={() => setYear(y)}
-                    className={`year${(y === currentYear) ? '-selected' : ''}`}
-                >
-                    {y}
-                </button>
-            </li>
-        ))}
-        {showPrevNext &&
-            <li key="next">
-                <button className="prevNext" onClick={() => setYear((currentYear === years.length) ? 1 : currentYear + 1)}>next</button>
-            </li>
-        }
-    </ul>
+export default ({
+  years, setYear, currentYear, showPrevNext = false,
+}) => (
+  <ul className="yearNav">
+    {showPrevNext
+      && <li key="prev"><button type="button" className="prevNext" onClick={() => setYear((currentYear === 1) ? (years.length) : currentYear - 1)}>previous</button></li>}
+    {years.map((y) => (
+      <li key={y}>
+        <button
+          type="button"
+          onClick={() => setYear(y)}
+          className={`year${(y === currentYear) ? '-selected' : ''}`}
+        >
+          {y}
+        </button>
+      </li>
+    ))}
+    {showPrevNext
+      && (
+      <li key="next">
+        <button type="button" className="prevNext" onClick={() => setYear((currentYear === years.length) ? 1 : currentYear + 1)}>next</button>
+      </li>
+      )}
+  </ul>
 );
