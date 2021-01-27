cd0f1f6f33e1c999e38a702c9bd432d7389ed085
Unknown <tamlin@mac.com>
2021-01-26 23:25:47 -0500

    allow robots in production

--- gatsby-config.js
+++ gatsby-config.js
@@ -32,7 +32,7 @@ module.exports = {
             policy: [{ userAgent: '*', disallow: ['/'] }],
           },
           production: {
-            policy: [{ userAgent: '*', disallow: '/' }],
+            policy: [{ userAgent: '*', allow: '/' }],
           },
         },
       },
