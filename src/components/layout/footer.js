ca3d963a58b18f089dc9eba6b8db98079b578706
Jonathan Williams <tamlin@mac.com>
2023-03-11 20:19:44 -0500

    footer changes, suppress footer graphic
    incomplete, should take the footer image out of the desktop size also

--- src/components/layout/footer.js
+++ src/components/layout/footer.js
@@ -7,6 +7,11 @@ import './footer.scss';
 function Footer() {
   return (
     <div id="footer">
+      <div className="org-name">
+        Massachusetts Institute of Technology
+        <br />
+        School of Architecture and Planning
+      </div>
       <NavBar />
       <div className="accessibility">
         <a href="https://accessibility.mit.edu">Accessibility</a>
