f623e6391542ae018678bcef587394c548d774fc
Jonathan Williams <tamlin@mac.com>
2026-02-18 10:29:58 -0500

    vision headline text change

--- src/components/vision/visionHeadline.js
+++ src/components/vision/visionHeadline.js
@@ -5,13 +5,19 @@ import './visionHeadline.scss';
 function VisionHeadline() {
   return (
     <h2 className="vision-text">
-      The Tangible Media Group, led by <br />
-      Professor Hiroshi Ishii, pursues the <br />
-      vision of Tangible Bits &amp; Radical Atoms<br />
-      to seamlessly couple the dual worlds  <br />
-      of bits and atoms by giving dynamic  <br />
-      physical form to digital information <br />
-      and computation.
+      The Tangible Media Group, led by
+      Professor Hiroshi Ishii, pursues the
+      vision of <b>Tangible Bits &amp; Radical Atoms</b> to seamlessly
+      couple the dual worlds
+      of bits and atoms by giving dynamic
+      physical form to digital information
+      and computation.<br />
+      As the Tangible Media Group approaches its next
+      chapter, <br />it revisits its origins in the vision
+      of <b>Seamless Telepresence</b> and introduces a new
+      vision: <b>TeleAbsence</b>—to connect people across
+      time, including one’s younger self, lost loved ones, and even the afterlife.<br />
+
     </h2>
   );
 }
